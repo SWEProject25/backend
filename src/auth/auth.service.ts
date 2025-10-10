ddfe0c87bd5784cf1e768501137391cafc66f9a3
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2025-10-10 19:26:35 +0300

    feat: add auth module with register feature

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  public async registerUser(createUserDto: CreateUserDto) {
+    const existingUser = await this.userService.findByEmail(
+      createUserDto.email,
+    );
+    if (existingUser) {
+      throw new BadRequestException('User is already exists');
+    }
+    return this.userService.create(createUserDto);
+  }
+}
