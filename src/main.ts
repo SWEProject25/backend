dc7fe234df1dad46e3c59b2a67509f3380900222
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2025-10-11 22:50:01 +0300

    docs: initialize swagger document builder

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const { PORT } = process.env;
@@ -11,6 +13,22 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swagger = new DocumentBuilder()
+    .setTitle('Hankers')
+    .addServer(`http://localhost:${PORT}`)
+    .setVersion('1.0')
+    .addSecurity('bearer', { type: 'http', scheme: 'bearer' })
+    .addBearerAuth()
+    .build();
+  const documentation = SwaggerModule.createDocument(app, swagger);
+  // http://localhost:PORT/swagger
+  SwaggerModule.setup('swagger', app, documentation);
+  writeFileSync(
+    './docs/api-documentation.json',
+    JSON.stringify(documentation, null, 2),
+  );
+
   try {
     await app.listen(PORT ?? 3001, () =>
       console.log(`Running in port ${PORT}`),
