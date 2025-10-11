016ff12748508d06b080c3be6b7ca4371ef8810a
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2025-10-11 14:24:21 +0300

    test: add prisma service unit testing

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -12,7 +12,30 @@ describe('PrismaService', () => {
     service = module.get<PrismaService>(PrismaService);
   });
 
+  afterEach(async () => {
+    await service.onModuleDestroy();
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('onModuleInit', () => {
+    it('should connent to the database', async () => {
+      const connectSpy = jest.spyOn(service, '$connect').mockResolvedValue();
+      await service.onModuleInit();
+      expect(connectSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('onModuleDestroy', () => {
+    it('should disconnect from the datebase', async () => {
+      const disconnectSpy = jest
+        .spyOn(service, '$disconnect')
+        .mockResolvedValue();
+      await service.onModuleDestroy();
+      expect(disconnectSpy).toHaveBeenCalled();
+    });
   });
 });
