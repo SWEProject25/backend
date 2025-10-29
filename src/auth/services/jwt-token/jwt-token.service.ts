aa0eb52e31e42a7b6074837d478ef259793197db
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2025-10-30 00:09:50 +0300

    fix(cookies): add domain

--- src/auth/services/jwt-token/jwt-token.service.ts
+++ src/auth/services/jwt-token/jwt-token.service.ts
@@ -23,6 +23,7 @@ export class JwtTokenService {
       secure: true,
       maxAge: ms(expiresIn),
       path: '/',
+      domain: '.myaddr.toos',
     };
 
     res.cookie('access_token', accessToken, cookieOptions);
