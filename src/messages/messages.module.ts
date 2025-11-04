046c60101b9e581e0eb068e082f2b9932fe8c539
“OmarNabil005” <omarnabil219@gmail.com>
2025-11-04 02:21:22 +0200

    feat: used redis to track active users

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -6,9 +6,12 @@ import { Services } from 'src/utils/constants';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { RedisModule } from 'src/redis/redis.module';
+import redisConfig from 'src/config/redis.config';
 
 @Module({
   imports: [
+    ConfigModule.forFeature(redisConfig),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -18,6 +21,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
       }),
     }),
     PrismaModule,
+    RedisModule,
   ],
   controllers: [MessagesController],
   providers: [
