b1832c1511b691d6d138532419b149ddcaa1de40
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2025-11-19 11:17:07 +0200

    fix(prisma): delete migration files

--- prisma/migrations/20251026184237_messages/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "profiles" DROP CONSTRAINT IF EXISTS "profiles_user_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
