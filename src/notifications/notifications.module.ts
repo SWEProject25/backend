316ba9055e6bfe00664483486d5877b64ab0fb23
ahmedGamalEllabban <ahmedg.ellabban339@gmail.com>
2025-11-29 12:38:55 +0200

    Add notification API endpoints

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationListener } from './events/notification.listener';
+import { NotificationsController } from './notifications.controller';
 import { Services } from 'src/utils/constants';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { FirebaseModule } from 'src/firebase/firebase.module';
 
 @Module({
   imports: [PrismaModule, FirebaseModule],
+  controllers: [NotificationsController],
   providers: [
     NotificationService,
     {
