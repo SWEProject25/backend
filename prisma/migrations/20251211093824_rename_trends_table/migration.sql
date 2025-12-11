37cc06c4351cae325f4223003088573b01432fb1
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2025-12-11 12:34:59 +0200

    fix(migrations): rename trends table

---
+++ prisma/migrations/20251211093824_rename_trends_table/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `HashtagTrend` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."HashtagTrend" DROP CONSTRAINT "HashtagTrend_hashtag_id_fkey";
+
+-- DropTable
+DROP TABLE "public"."HashtagTrend";
+
+-- CreateTable
+CREATE TABLE "hashtag_trends" (
+    "id" SERIAL NOT NULL,
+    "hashtag_id" INTEGER NOT NULL,
+    "post_count_1h" INTEGER NOT NULL,
+    "post_count_24h" INTEGER NOT NULL,
+    "post_count_7d" INTEGER NOT NULL,
+    "trending_score" DOUBLE PRECISION NOT NULL,
+    "calculated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "hashtag_trends_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "hashtag_trends_trending_score_idx" ON "hashtag_trends"("trending_score");
+
+-- CreateIndex
+CREATE INDEX "hashtag_trends_hashtag_id_idx" ON "hashtag_trends"("hashtag_id");
+
+-- AddForeignKey
+ALTER TABLE "hashtag_trends" ADD CONSTRAINT "hashtag_trends_hashtag_id_fkey" FOREIGN KEY ("hashtag_id") REFERENCES "Hashtag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
