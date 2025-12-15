655498c2c3d2913cc6e1c531211c7719b90c1ac9
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2025-12-15 20:39:44 +0200

    fix(username): change validations for username

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -34,7 +34,7 @@ export class UpdateUserDto {
   @IsString()
   @MinLength(3, { message: 'Username must be at least 3 characters long' })
   @MaxLength(50, { message: 'Username must be at most 50 characters long' })
-  @Matches(/^[a-zA-Z](?!.*[_.]{2})[a-zA-Z0-9._]*[a-zA-Z0-9]$/, {
+  @Matches(/^[a-zA-Z](?!.*[_.-]{2})[a-zA-Z0-9._-]*[a-zA-Z0-9]$/, {
     message:
       'Username must start with a letter, end with a letter or number, and can only contain letters, numbers, dots, and underscores — without consecutive dots or underscores.',
   })
--- src/user/dto/update-username.dto.ts
+++ src/user/dto/update-username.dto.ts
@@ -6,7 +6,7 @@ export class UpdateUsernameDto {
   @IsNotEmpty({ message: 'Username is required' })
   @MinLength(3, { message: 'Username must be at least 3 characters long' })
   @MaxLength(50, { message: 'Username must be at most 50 characters long' })
-  @Matches(/^[a-zA-Z](?!.*[_.]{2})[a-zA-Z0-9._]+$/, {
+  @Matches(/^[a-zA-Z](?!.*[_.-]{2})[a-zA-Z0-9._-]+$/, {
     message:
       'Username must start with a letter and can only contain letters, numbers, dots, and underscores — without consecutive dots or underscores.',
   })
